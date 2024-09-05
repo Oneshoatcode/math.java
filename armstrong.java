ea04eed3005439219ae56c1cec1066ee08f873d7
Oneshoatcode <sonukumar769591@gmail.com>
2024-09-05 10:57:59 +0530

    Update armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -8,14 +8,15 @@ public class armstrong {
     int armstrong=0;
     int original=n;
     int lastdig;
-    while(n>0){
+    while(n>0)
+    {
     lastdig=n%10;
-    System.out.println(lastdig);
-    armstrong+=lastdig*lastdig*lastdig;
+    System.out.println(lastdig);//last digit of a number
+    armstrong+=lastdig*lastdig*lastdig;//Here,this is cube of the number and sum with armstron 
     n=n/10;
     }
     System.out.println(armstrong);
-    if (armstrong==original) {
+    if (armstrong==original) { //check with armstong with original number
         System.out.println("number is arm strong");
     }
 else{
