259888e87ceffd14c3f0a803c633214f9baf24a0
Oneshoatcode <sonukumar769591@gmail.com>
2024-09-06 21:46:03 +0530

    Update cheack_answer.java

--- cheack_answer.java
+++ cheack_answer.java
@@ -9,10 +9,10 @@ public static void main(String[] args) {
     a=sc.nextInt();
     System.out.println("enter your number b ");
     b=sc.nextInt();
-    int sum=a+b;
+    int sum=a+b;//add a andc b
     int user_ans;
     System.out.println("enter user  input");
-    user_ans=sc.nextInt();
+    user_ans=sc.nextInt();//enter user input to match with sum if user input is correct than user gusse is right
    
     if(sum==user_ans){
         System.out.println("correct answer");
